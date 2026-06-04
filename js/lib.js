4291e47ce835e24942427495e1ff6bae75ace37d
THUY AN <tranthithuyan22052007@gmail.com>
2026-06-04 16:39:15 +0700

    up

--- js/lib.js
+++ js/lib.js
@@ -1,1 +1,84 @@
 /*javacript*/
+function inBCC(n)
+{
+    let result = "";
+    let i=1;
+    while(i<=10)
+    {
+       result += `${n} x ${i} = ${n*i} <br>`;
+       i++;
+    }  
+    document.getElementById("result").innerHTML = result;  
+}
+
+function addItem(name, price, description, link, image)
+{
+    //Tạo khung chứa container-item
+    const item = document.createElement("div"); //<div></div>
+
+    item.setAttribute("class", "container-item");
+
+    //Trong 1 item có khung chứa container-image và container-info
+
+    //Tạo khung chứa container-image
+    const containerImage = document.createElement("div");
+    containerImage.setAttribute("class", "container-image");
+
+    //Tạo 1 ảnh
+    const imageProduct = document.createElement("img");
+    imageProduct.setAttribute("src", image); // truyền tham số image
+    imageProduct.setAttribute("alt", name);
+    imageProduct.setAttribute("style", "width:100%; max-width:150px;");
+
+    //Chèn đối tượng ảnh vào khung chứa ảnh
+    containerImage.appendChild(imageProduct);
+
+    //Tạo khung chứa container-info
+    const containerInfo = document.createElement("div");
+    containerInfo.setAttribute("class", "container-info");
+
+    //Tạo 4 đứa con
+    const nameProduct = document.createElement("p");
+    nameProduct.innerHTML = name;
+
+    const priceProduct = document.createElement("p");
+    priceProduct.innerHTML = price;
+
+    const descProduct = document.createElement("p");
+    descProduct.innerHTML = description;
+
+    const linkProduct = document.createElement("a");
+    linkProduct.innerHTML = "Xem chi tiết";
+    linkProduct.setAttribute("href", link);
+
+    //Thêm con vào khung chứa info
+    containerInfo.appendChild(nameProduct);
+    containerInfo.appendChild(priceProduct);
+    containerInfo.appendChild(descProduct);
+    containerInfo.appendChild(linkProduct);
+
+    //Đưa khung ảnh vào item
+    item.appendChild(containerImage);
+
+    //Đưa khung info vào item
+    item.appendChild(containerInfo);
+
+    //Đưa khung chứa container-product-list vào container
+    document.getElementById("container-product-list").appendChild(item);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
